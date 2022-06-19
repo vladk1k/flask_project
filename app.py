dd4d64559a70ae73e3eede0c0dd28aba34cea084
vladk1k <vlad02658@gmail.com>
2022-06-19 16:59:15 +0300

    flask project

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+app.secret_key = 'some secret_salt2281234'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+manager = LoginManager(app)
 
 
 class Article(db.Model):
@@ -19,11 +23,27 @@ class Article(db.Model):
         return '<Article %r>' % self.id
 
 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
+
+
+@manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 
 @app.route('/')
 @app.route('/home')
 def index():
     return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 
 @app.route('/posts')
 def posts():
@@ -32,6 +52,7 @@ def posts():
 
 
 @app.route('/posts/<int:id>')
+@login_required
 def posts_detail(id):
     article = Article.query.get(id)
     return render_template("post_detail.html", article=article)
@@ -39,6 +60,7 @@ def posts_detail(id):
 
 
 @app.route('/posts/<int:id>/delete')
+@login_required
 def posts_delete(id):
     article = Article.query.get_or_404(id)
     try:
@@ -50,6 +72,7 @@ def posts_delete(id):
 
 
 @app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
+@login_required
 def post_update(id):
     article = Article.query.get(id)
     if request.method == "POST":
@@ -67,6 +90,7 @@ def post_update(id):
 
 
 @app.route('/create_article', methods=['POST', 'GET'])
+@login_required
 def create_article():
     if request.method == "POST":
         title = request.form['title']
@@ -83,6 +107,64 @@ def create_article():
             return "При добавлении статьи произошла ошибка"
     else:
         return render_template("create_article.html")
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login_page():
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if login and password:
+        user = User.query.filter_by(login=login).first()
+
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+
+            #next_page = request.args.get('next')
+
+            return redirect('/')
+        else:
+            flash('Логин или пароль введены неверно')
+    else:
+        flash('Поля должны быть заполнены')
+    return render_template('login.html')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    login = request.form.get('login')
+    password = request.form.get('password')
+    password2 = request.form.get('password2')
+
+    if request.method == 'POST':
+        if not (login or password or password2):
+            flash('Please, fill all fields')
+        elif password != password2:
+            flash('Passwords are not equal!')
+        else:
+            hash_pwd = generate_password_hash(password)
+            new_user = User(login=login, password=hash_pwd)
+            db.session.add(new_user)
+            db.session.commit()
+
+            return redirect(url_for('login_page'))
+
+    return render_template('register.html')
+
+
+@app.route("/logout", methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('posts'))
+
+
+@app.after_request
+def redirect_to_signin(response):
+    if response.status_code == 401:
+        return redirect(url_for('login_page') + '?next' + request.url)
+
+    return response
 
 
 if __name__ == '__main__':
